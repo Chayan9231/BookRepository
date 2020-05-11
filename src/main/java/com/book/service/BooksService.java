4895dfdd2d4f9c2528c3c6b88853d847c85fb7a1
Chayan9231 <duttachayan600@gmail.com>
2020-05-11 13:14:08 +0530

    ** Exception Handling Added **

--- src/main/java/com/book/service/BooksService.java
+++ src/main/java/com/book/service/BooksService.java
@@ -5,40 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.book.exception.BookNotFoundException;
 import com.book.model.Books;
 import com.book.repository.BooksRepository;
-
-
 
 @Service
 public class BooksService {
 	@Autowired
 	BooksRepository booksRepository;
 
-	
 	public List<Books> getAllBooks() {
 		List<Books> books = new ArrayList<Books>();
 		booksRepository.findAll().forEach(books1 -> books.add(books1));
 		return books;
 	}
 
-	
 	public Books getBooksById(int id) {
-		return booksRepository.findById(id).get();
-	}
 
+		Books book = null;
+		try {
+			book = booksRepository.findById(id).get();
+		} catch (Exception e) {
+			throw new BookNotFoundException("Book Not Found : " + id);
+		}
+		return book;
+	}
 
 	public void saveOrUpdate(Books books) {
 		booksRepository.save(books);
 	}
 
-
-
 	public void delete(int id) {
 		booksRepository.deleteById(id);
 	}
-
-
 
 	public void update(Books books, int bookid) {
 		booksRepository.save(books);
